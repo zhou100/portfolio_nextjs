8867449c45ca74bca4040712d39a79440fc3c104
zhou100 <zhouyujun001@gmail.com>
2025-02-08 22:11:01 -0800

    Fix: Replace next.config.ts with next.config.js

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Add only supported configuration options
-  reactStrictMode: true,
-  swcMinify: true
-};
-
-export default nextConfig;
